820c09a7e4c3bcad25a3b3ad437e2b0f24edbaf6
Eduard Canet <edtasixm06@gmail.com>
2019-03-11 13:13:27 +0100

    postgres

--- postgres:detach/install.sh
+++ postgres:detach/install.sh
@@ -14,8 +14,8 @@ echo "jupiter" | passwd --stdin postgres
 useradd edtasixm06
 echo "edtasixm06" | passwd --stdin edtasixm06
 
-cp /opt/docker/pg_hba.conf /var/lib/pgsql/data/pg_hba.conf
-cp /opt/docker/postgresql.conf /var/lib/pgsql/data/postgresql.conf
+#cp /opt/docker/pg_hba.conf /var/lib/pgsql/data/pg_hba.conf
+#cp /opt/docker/postgresql.conf /var/lib/pgsql/data/postgresql.conf
 
 su -l postgres
 OOMScoreAdjust=-1000
@@ -28,26 +28,31 @@ pg_ctl initdb
 /usr/bin/pg_ctl -D /var/lib/pgsql/data -l logfile start
 exit
 
+cp /opt/docker/pg_hba.conf /var/lib/pgsql/data/pg_hba.conf
+cp /opt/docker/postgresql.conf /var/lib/pgsql/data/postgresql.conf
+
 # --------------------------
 /usr/libexec/postgresql-ctl restart
 su -l postgres
 psql template1
 CREATE USER edtasixm06;
-#grant ALL ON DATABASE template1 TO edtasixm06;
 create database training;
 grant ALL ON DATABASE training to edtasixm06;
-grant ALL on oficinas, pedidos, productos, repventas, clientes to edtasixm06;
 \q
 psql training
 \i /opt/docker/training/pedidos.sql
 \i /opt/docker/training/productos.sql
 \i /opt/docker/training/clientes.sql
 \i /opt/docker/training/repventas.sql
+\i /opt/docker/training/oficinas.sql
 \copy pedidos from /opt/docker/training/pedidos.dat 
 \copy productos from /opt/docker/training/productos.dat 
 \copy clientes from /opt/docker/training/clientes.dat  
 \copy repventas from /opt/docker/training/repventas.dat 
 \copy oficinas from /opt/docker/training/oficinas.dat  
+grant ALL on oficinas, pedidos, productos, repventas, clientes to edtasixm06;
+\q
 exit
 # ------------------------------------------------------------
+
 
